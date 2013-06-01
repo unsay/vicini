ad783c6ac94b7f8ad6118fe429a26a241a02362a
Cory Preus <cory@softwareforgood.com>
2013-06-01 14:12:08 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vicini::Application.config.secret_key_base = 'b7efabf6b050110361526b2867b4659795561f5fbdfc90d1069ab7101be0f578b3a6fbe6dbc58cd9e0cd559a62c03754a866c975481cbe5eaa0e2be1c171462b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vicini::Application.config.session_store :encrypted_cookie_store, key: '_vicini_session'

ef4440b26332a2aa4ba1de904f8437c6a10b8b7d
Cory Preus <cory@softwareforgood.com>
2013-06-01 14:32:45 -0500

    Additional setup.

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module Vicini
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework :rspec
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,18 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter 'vendor' # Don't include vendored stuff
+  add_group 'Jobs', 'app/jobs'
+  add_group 'Decorators', 'app/decorators'
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/rspec'
+require 'rspec_candy/helpers'
+require 'vcr'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -13,6 +23,10 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+  config.include Sorcery::TestHelpers::Rails
+  # config.include AuthenticationForFeatureRequest, type: :feature
+  #
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:

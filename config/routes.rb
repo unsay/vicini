73c1df788d8007e32e2bc80804b2e6c7aaf0654b
Jacob Dalton <jacobrdalton@gmail.com>
2013-06-02 14:47:52 -0500

    built views for map

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Vicini::Application.routes.draw do
   resources :users
+
+  resources :users do
+    resources :messages
+  end
   root to: 'welcome#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130601232622) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "listenings", force: true do |t|
     t.integer  "user_id",    null: false
     t.integer  "radius"

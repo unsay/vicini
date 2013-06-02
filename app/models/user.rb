303ddc2361c13f28e7e4fda4c1ead2c1c553a4c5
Cory Preus <cory@softwareforgood.com>
2013-06-02 13:46:23 -0500

    Full address for geocoding.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  geocoded_by :address1
+
+  geocoded_by :full_address_for_geocoding
 
   after_validation :geocode
 
   has_many :messages
   has_many :listenings
   has_many :speakings
+
+private
+  
+  def full_address_for_geocoding
+    "#{address1} #{address2} #{city} #{state} #{zip}"
+  end
+
 end
+
